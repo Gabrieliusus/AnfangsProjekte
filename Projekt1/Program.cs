0a7dd5423a1aa47bd8eaa3ce877fc1362e7a02d5
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2024-09-25 12:49:54 +0200

    Programmierauftrag 01

--- Projekt1/Program.cs
+++ Projekt1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, SWP!!");
+string UserInput = " ";
+while (true)
+{
+    UserInput = Console.ReadLine();
+    Console.WriteLine(UserInput);
+    if (UserInput == "beenden") {  break; }
+}
