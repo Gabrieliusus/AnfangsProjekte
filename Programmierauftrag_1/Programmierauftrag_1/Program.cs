f33ac1dce77a4bba2e4665173e4b24fac4ea1d8e
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2024-11-28 22:01:41 +0100

    Programmierauftrag_1 DONE

---
+++ Programmierauftrag_1/Programmierauftrag_1/Program.cs
@@ -0,0 +1,12 @@
+﻿bool isTrue = true;
+
+while (isTrue)
+{
+    Console.WriteLine("Geben Sie einen beliebigen Text ein:");
+    string Input = Console.ReadLine();
+    Console.WriteLine(Input);
+    if (Input == "Beenden" || Input == "beenden")
+    {
+        isTrue = false;
+    }
+}
