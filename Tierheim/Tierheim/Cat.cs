cb309e552bd9264a1906e31a457b38ea4edd84f7
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2024-11-29 10:23:32 +0100

    Animal done for now

---
+++ Tierheim/Tierheim/Cat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vererbung;
+
+namespace Vererbung
+{
+
+    class Cat : Animal
+    {
+        public string setColor { get; set; }
+       
+        public int setAge { get { return (DateTime.Now.Year - _Birthdate.Year); } }
+        public Cat(DateTime birthdate, string Color = "Nicht Verfügbar", string name = "Nicht Verfügbar") : base(name, birthdate) 
+        {
+            setColor = Color;
+        }
+        public override string ToString()
+        {
+            return $"Name :{Name}, Farbe: {setColor}, Alter: {setAge}";
+        }
+    }
+}
