763f93a6ac012973aba695b1ada0901aeab5b5c8
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2025-01-24 10:04:37 +0100

    erster commit

---
+++ fünfertest/fünfertest unitest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace fünfertest_unitest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
