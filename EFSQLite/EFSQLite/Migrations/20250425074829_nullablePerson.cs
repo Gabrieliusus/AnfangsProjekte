da90c1f2be61604440f391488e02c110de87fa1d
Gabriel Mayer <gabriel.mayer@student.htldornbirn.at>
2025-04-25 09:49:53 +0200

    DB Migration DONE

---
+++ EFSQLite/EFSQLite/Migrations/20250425074829_nullablePerson.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFSQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class nullablePerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PLZ",
+                table: "Staedte",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Alter",
+                table: "Personen",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PLZ",
+                table: "Staedte");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Alter",
+                table: "Personen",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
